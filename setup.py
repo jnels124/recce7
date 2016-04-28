35fc7126d83bddb8f468873320da6b5dea8264b8
Jesse <jnels1242012@gmail.com>
2016-04-27 19:10:04 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
-import platform
 
 setup(name='recce7',
       version='1.0',
-      description='Honeypot',
+      description='Report Server',
       author='Jesse Nelson',
       author_email='jnels124@msudenver.edu',
-      data_files=[('/etc/recce7/configs/', ['config/plugins.cfg'])],
-      scripts=['startHoneyPot.sh'],
-      packages=['framework', 'plugins', 'database', 'common'])
+      data_files=[('/etc/recce7/configs/', ['config/plugins.cfg', 'config/global.cfg']),
+                  ('/usr/local/sbin/recce7/database/sql_scripts', ['database/sql_scripts/sessions.sql'])],
+      scripts=['startReportServer.sh'],
+      packages=['framework', 'plugins', 'database', 'database.sql_scripts', 'common', 'reportserver', 'reportserver.dao', 'reportserver.manager', 'reportserver.server'])
