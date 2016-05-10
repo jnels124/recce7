163f238365dbfc9ed982e4685a1b9628b366d71e
Jesse <jnels1242012@gmail.com>
2016-05-09 22:48:09 -0600

    Testing

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(name='recce7',
       install_requires=['python-dateutil', 'p0f', 'requests'],
       data_files=[('/etc/recce7/configs/', ['config/plugins.cfg', 'config/global.cfg']),
                   ('/usr/sbin/recce7/database/sql_scripts', ['database/sql_scripts/sessions.sql'])],
-      scripts=['startReportServer.sh',  'recce7.sh', 'auth'],
+      scripts=['startReportServer.sh',  'recce7.sh', 'authbind_recce7.sh'],
       packages=['framework', 'plugins', 'database', 'common', 'recon', 'reportserver',
                 'reportserver.dao', 'reportserver.manager', 'reportserver.server'])
